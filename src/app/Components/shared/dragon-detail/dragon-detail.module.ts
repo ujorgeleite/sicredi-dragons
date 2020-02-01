8a720c32955435f51db2644859a070ab3170bbde
ujorgeleite <jorgefernandodamasio@gmail.com>
2020-02-01 09:36:14 -0300

    feat(component): add dragon detail component

---
+++ src/app/Components/shared/dragon-detail/dragon-detail.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DragonDetailComponent } from './dragon-detail.component';
+
+
+
+@NgModule({
+  declarations: [
+    DragonDetailComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    DragonDetailComponent
+  ]
+})
+export class DragonDetailModule { }
