6b4794ff66d1a5921b15205e378a1fa4fa64d4a1
ujorgeleite <jorgefernandodamasio@gmail.com>
2020-02-02 19:10:32 -0300

    feat(module): add new routes and modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,23 @@ import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListComponent } from './Components/List/list.component';
 import { DragonDetailComponent } from './Shared/components/dragon-detail/dragon-detail.component';
+import { LoginComponent } from './Components/login/login.component';
+import { AuthGuardService } from './Shared/services/auth-guard.service';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
+
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/list', pathMatch: 'full' },
-  { path: 'list', component: ListComponent },
-  { path: 'detail/:id', component: DragonDetailComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'list', component: ListComponent, canActivate: [AuthGuardService] },
+  { path: 'detail/:id', component: DragonDetailComponent, canActivate: [AuthGuardService] },
+  { path: 'login', component: LoginComponent },
+  { path: '404', component: NotFoundComponent },
+  { path: '**', redirectTo: '/404' }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
