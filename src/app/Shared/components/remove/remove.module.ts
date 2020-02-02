b098974315dc00cad256d1e8912401eeb0122c80
ujorgeleite <jorgefernandodamasio@gmail.com>
2020-02-01 23:56:13 -0300

    feat(remove): add remove component

---
+++ src/app/Shared/components/remove/remove.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RemoveComponent } from './remove.component';
+
+
+
+@NgModule({
+  declarations: [
+    RemoveComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    RemoveComponent
+  ]
+})
+export class RemoveModule { }
